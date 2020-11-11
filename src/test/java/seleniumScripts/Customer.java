0ad9ee23927fbd7844a5741106a69b0d84925690
kiran <kiranhalagi7@gmail.com>
2020-11-11 12:02:31 +0530

    selenium commit

---
+++ src/test/java/seleniumScripts/Customer.java
@@ -0,0 +1,15 @@
+package seleniumScripts;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		createcustomer();
+
+	}
+	
+	public static void createcustomer()
+	{
+		System.out.println("Customer created successfully");
+	}
+
+}
---
+++ src/test/java/seleniumScripts/User.java
@@ -0,0 +1,16 @@
+package seleniumScripts;
+
+public class User {
+
+	public static void main(String[] args) {
+		createuser();
+
+	}
+
+	
+	public static void createuser()
+	{
+		System.out.println("user created successfully");
+	}
+
+}
